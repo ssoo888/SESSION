39c4b7161368bf861a9c0b2e6a08c66378061bcc
ssoo888 <trinidadaku@gmail.com>
2025-02-26 11:05:29 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 
-
 class Config(object):
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7365923851:AAEGwQSc7LMONLGCAbRqVNQGtfAoyBewmII")
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN")
+    APP_ID = os.environ.get("APP_ID")
+    API_HASH = os.environ.get("API_HASH")
 
-    APP_ID = int(os.environ.get("APP_ID", 21627756))
-
-    API_HASH = os.environ.get("API_HASH", "fe77fbf0cae9f7f5ece37659e2466cf1")
+    if not TG_BOT_TOKEN or not APP_ID or not API_HASH:
+        raise ValueError("❌ خطأ: تأكد من ضبط متغيرات البيئة TG_BOT_TOKEN, APP_ID, و API_HASH")
