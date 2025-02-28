5433ff1d97354e5ba5d0e2d530ab840f44050851
ssoo888 <trinidadaku@gmail.com>
2025-02-28 12:29:09 +0300

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-class Config(object):
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN")
-    APP_ID = os.environ.get("APP_ID")
-    API_HASH = os.environ.get("API_HASH")
-
-    if not TG_BOT_TOKEN or not APP_ID or not API_HASH:
-        raise ValueError("❌ خطأ: تأكد من ضبط متغيرات البيئة TG_BOT_TOKEN, APP_ID, و API_HASH")
